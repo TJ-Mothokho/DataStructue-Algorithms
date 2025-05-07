abc4c730ab3d66d24a97c788cca9246e45194b1d
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2025-05-07 14:10:36 +0100

    Added Bubble Sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 64, 34, 25, 12, 22, 11, 90, 5 };
+
+            bool swapped;
+            int count = 0;
+
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                swapped = false;
+                
+                for(int j = 0; j < array.Length-1-i; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        int temp = array[j+1];
+                        array[j+1] = array[j];
+                        array[j] = temp;
+
+                        swapped = true;
+                    }
+                }
+
+                count++;
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", array));
+
+            Console.ReadLine();
+        }
+    }
+}
